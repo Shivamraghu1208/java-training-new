6128633c0c5e9dc1d249e938a91b9307d60566e6
Shivam Raghuwanshi <shivam.raghuwanshi@aurx.co>
2024-04-30 16:16:46 +0530

    add a java file

---
+++ src/com/training/Hashcode.java
@@ -0,0 +1,25 @@
+package com.training;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Hashcode {
+    public static void main(String[] args) {
+     /* Map<String,String> map =new HashMap<>();
+      map.put("Dev","Shivam");
+      map.put("Dev","vISHNU");
+        map.put("Dev","Akash");
+        for (Map.Entry entry:map.entrySet()) {
+            System.out.println(entry.getKey()+" "+entry.getValue());
+        }*/
+        Map<Demo,String> map =new HashMap<>();
+        map.put(new Demo(2,"3"),"Shivam");
+        map.put(new Demo(3,"3"),"Sumit");
+        map.put(new Demo(2,"3"),"vishnu");
+//        map.put(new Sumit("SHivam"),"Sumit piapriya");
+//        map.put(new Sumit("Sumit"),"Sumit indore");
+        for (Map.Entry entry:map.entrySet()) {
+            System.out.println(entry.getKey()+" "+entry.getValue());
+        }
+    }
+}
