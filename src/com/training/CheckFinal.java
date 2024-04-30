345541c761f45f8ef769d2433e31c5cc10e825b6
Shivam Raghuwanshi <shivam.raghuwanshi@aurx.co>
2024-04-30 14:21:49 +0530

    new commit

---
+++ src/com/training/CheckFinal.java
@@ -0,0 +1,18 @@
+package com.training;
+
+public class CheckFinal {
+
+    public final void m1()
+    {
+        System.out.println("heoll");
+    }
+}
+//
+// class B extends Ch
+//{
+//    public final void m1()
+//    {
+//        System.out.println("heoll");
+//    }
+//
+//}
