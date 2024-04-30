f6b3cf8c29dc2f38a41dd94b6a3251cc4b07ed52
Shivam Raghuwanshi <shivam.raghuwanshi@aurx.co>
2024-04-30 17:09:48 +0530

    ad new file

---
+++ src/com/training/MyAspect.java
@@ -0,0 +1,17 @@
+/*
+package com.training;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class MyAspect {
+
+    @Before("execution(* *(..)) && target(MyMarkerInterface)")
+    public void callMethodAutomatically() {
+        System.out.println("Method called automatically upon object creation.");
+        // Add your functionality here
+    }
+}*/
