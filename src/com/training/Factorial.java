fbb4bede393589475833dfa128869b10c8ab5fc9
Shivam Raghuwanshi <shivam.raghuwanshi@aurx.co>
2024-04-30 15:59:00 +0530

    adding java program

---
+++ src/com/training/Factorial.java
@@ -0,0 +1,18 @@
+package com.training;
+
+public class Factorial {
+
+    public static int fact(int n)
+    {
+        if(n==0)
+        {
+            return 1;
+        }
+        return n*fact(n-1);
+
+    }
+    public static void main(String[] args) {
+        int fact = fact(5);
+        System.out.println(fact);
+    }
+}
